93d812be303b0769163a1e348a065799807af44f
ogbonnachinonso <ogbonnachinonsoubadire@gmail.com>
2021-03-05 15:54:59 +0100

    more update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DATABASE_LOCAL, {
+    const conn = await mongoose.connect(process.env.DATABASE_REMOTE, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
